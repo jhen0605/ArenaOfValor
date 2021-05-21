895930d1da482685da8bec17b2b45bc436dc4cb0
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-05-21 19:54:29 +0800

    Initial Commit

---
+++ ArenaOfValor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArenaOfValor
+//
+//  Created by 簡吟真 on 2021/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
